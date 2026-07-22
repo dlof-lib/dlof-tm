8a9756a0c7e35fa80cc3292872903b6a2a6eb3c8
dlof-lib <javadlof8@gmail.com>
2026-07-23 00:42:05 +0300

    Initial commit from ZIP upload

---
+++ app/src/main/kotlin/org/dlof/tm/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package org.dlof.tm.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DlofGreenDark = Color(0xFF0F5132)
+val DlofGreen = Color(0xFF1E8E3E)
+val DlofGreenLight = Color(0xFF34C759)
+val DlofBlack = Color(0xFF101410)
+val DlofWhite = Color(0xFFFFFFFF)
+val DlofSurfaceDark = Color(0xFF141B14)
+val DlofErrorRed = Color(0xFFB3261E)
