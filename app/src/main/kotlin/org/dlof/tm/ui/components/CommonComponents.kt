9c2e0598d0297d23d99449ace431a2cef82b1ca1
dlof-lib <javadlof8@gmail.com>
2026-07-23 13:25:59 +0300

    Initial commit from ZIP upload

--- app/src/main/kotlin/org/dlof/tm/ui/components/CommonComponents.kt
+++ app/src/main/kotlin/org/dlof/tm/ui/components/CommonComponents.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
@@ -69,6 +70,7 @@ fun HexColorField(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun <T> LabeledDropdown(
     label: String,
